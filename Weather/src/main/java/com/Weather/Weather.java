703e38ab137b0af5c3fd840e44a8c54e4614d0a4
cowlog <cowlog.li@gmail.com>
2018-02-02 10:01:01 +0800

    {Weather} [Add] Common.HttpRequest Add HttpRequest methods.

--- Weather/src/main/java/com/Weather/Weather.java
+++ Weather/src/main/java/com/Weather/Weather.java
@@ -1,10 +1,12 @@
 package com.Weather;
 
+import com.Weather.Common.HttpRequest;
+
 /**
  * Created by cowlog on 18-2-2.
  */
 public class Weather {
     public static void main(String[] args) {
-    System.out.println("Hello, world!");
+        System.out.println("Hello, world!");
     }
 }
