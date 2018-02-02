a26d145007c0a8762c02bf5b4f6cf66653a6f31b
cowlog <cowlog.li@gmail.com>
2018-02-02 12:44:55 +0800

    {ZipCode} [Init] Init project

---
+++ ZipCode/src/main/java/com/ZipCode/ZipCode.java
@@ -0,0 +1,12 @@
+package com.ZipCode;
+
+/**
+ * Created by cowlog on 18-2-2.
+ * Implement Zip code.
+ */
+
+public class ZipCode {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
