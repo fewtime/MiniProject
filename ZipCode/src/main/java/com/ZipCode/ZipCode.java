2d76f93ff933e97cdcac4b6711e16141c67585fa
cowlog <cowlog.li@gmail.com>
2018-02-02 13:14:13 +0800

    {ZipCode} [Add] A demo.

--- ZipCode/src/main/java/com/ZipCode/ZipCode.java
+++ ZipCode/src/main/java/com/ZipCode/ZipCode.java
@@ -1,12 +1,47 @@
 package com.ZipCode;
 
+import com.ZipCode.Base.Result;
+import com.ZipCode.Base.RootObject;
+import com.ZipCode.Common.HttpRequest;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Created by cowlog on 18-2-2.
- * Implement Zip code.
+ * Implement Zip code (demo).
  */
 
 public class ZipCode {
+    private static final String apiURL = "http://api.avatardata.cn/PostNumber/QueryPostnumber";
+    private static final String apiKey = null;
+    private static final String apiRow = "50";
+    private static final String apiPage = "1";
+
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        Scanner sc = new Scanner(System.in);
+        String postCode;
+        Gson gson = new Gson();
+        do {
+            System.out.println("Enter a zip code('q' to quit): ");
+            postCode = sc.nextLine();
+            String res = HttpRequest.sendGet(apiURL, "key=" + apiKey
+                    + "&postnumber=" + postCode
+                    + "&page=" + apiPage
+                    + "&row=" + apiRow
+            );
+            RootObject rootObject = gson.fromJson(res, RootObject.class);
+            if (rootObject.getErrorCode() == 0) {
+                ArrayList<Result> arr = rootObject.getResult();
+                for (Result result : arr) {
+                    System.out.println(result.getProvince()
+                            + " " + result.getCity()
+                            + " " + result.getDistrict()
+                            + " " + result.getAddress()
+                            + " " + result.getJd());
+                }
+            }
+        } while (!postCode.equals("q"));
     }
 }
