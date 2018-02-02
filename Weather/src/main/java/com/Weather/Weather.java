d73d9add59b8c3c2096b5b4493542bc3b753f235
cowlog <cowlog.li@gmail.com>
2018-02-02 09:53:38 +0800

    {Weather}[Init]

---
+++ Weather/src/main/java/com/Weather/Weather.java
@@ -0,0 +1,10 @@
+package com.Weather;
+
+/**
+ * Created by cowlog on 18-2-2.
+ */
+public class Weather {
+    public static void main(String[] args) {
+    System.out.println("Hello, world!");
+    }
+}
