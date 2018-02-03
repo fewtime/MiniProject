ca2a5c6e4f3bd6dcae15dd4f2698e950956af8cf
cowlog <cowlog.li@gmail.com>
2018-02-03 10:24:58 +0800

    {NTPTime} [Add] Init project.

---
+++ NTPTime/src/main/java/com/NTPTime/NTPTime.java
@@ -0,0 +1,11 @@
+package com.NTPTime;
+
+/**
+ * Created by cowlog on 18-2-3.
+ * Implement NTP time demo.
+ */
+public class NTPTime {
+    public static void main(String[] args) {
+        System.out.println("Hello, world.");
+    }
+}
