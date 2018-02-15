711041a6a66cb8c0acb41146c76a35c7bfd1b6bd
cowlog <cowlog.li@gmail.com>
2018-02-15 19:14:27 +0800

    {WebBrowser}[Add] gitignore file

---
+++ WebBrowser/src/main/java/com/WebBrowser/main/WebBrowser.java
@@ -0,0 +1,10 @@
+package com.WebBrowser.main;
+
+/**
+ * Created by cowlog on 18-2-15.
+ */
+public class WebBrowser {
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+    }
+}
